729f60bcc1147f4434e6492c096f78ed8c8d653e
Paulo <paulofilhobarroso@gmail.com>
2023-04-11 16:09:32 -0300

    Fix: Adjusting return of first dfs, adding comments in the terminal.

--- InstagramCluster.java
+++ InstagramCluster.java
@@ -19,19 +19,19 @@ public class InstagramCluster {
     }
 
     /**
-     * Primeiro DFS, retorna a lista na ordem de vertices finalizados.
+     * Primeiro DFS, retorna a lista na ordem de vertices visitados.
      */
-    public List<String> dfs() {
+    public Set<String> dfs() {
         // Gera estruturas de dados para finalizados e visitados.
         LinkedList<String> finished = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new LinkedHashSet<>();
         for (String node : graph.keySet()) { // Itera cada vertice no grafo
             if (!visited.contains(node)) { // Verifica se vertice já foi visitado.
                 dfsVisit(node, visited, finished); // Realiza dfs no vertice visitado.
             }
         }
-        // Após toda iteração e recursividade, retorna a lista na ordem dos que foram finalizados.
-        return finished;
+        // Após toda iteração e recursividade, retorna a lista na ordem dos que foram visitados.
+        return visited;
     }
 
     /**
@@ -39,12 +39,14 @@ public class InstagramCluster {
      */
     private void dfsVisit(String node, Set<String> visited, LinkedList<String> finished) {
         visited.add(node); // Adiciona vertice na lista de visitados.
+        System.out.println("Visitados ->> " + visited);
         for (String neighbor : graph.get(node)) { // Itera cada vizinho do vertice visitado. Indo mais fundo possivel
             if (!visited.contains(neighbor)) { // Verifica se vizinho já foi marcado como visitado
                 dfsVisit(neighbor, visited, finished); // Faz recursividade para marcar a maior quantidade de vertices possiveis como visitados.
             }
         }
         finished.addFirst(node); // Coloca vertice depois de iterado para dentro da lista de vertices finalizados.
+        System.out.println("Finalizados ->> " + finished);
     }
 
     /**
@@ -67,14 +69,15 @@ public class InstagramCluster {
     /**
      * Realiza DFS especifíca da transposta e retornando o resultado com os componentes finais.
      */
-    public List<List<String>> dfsTranspose(Map<String, List<String>> transpose, List<String> sortedNodes) {
+    public List<List<String>> dfsTranspose(Map<String, List<String>> transpose, Set<String> sortedNodes) {
         List<List<String>> result = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new LinkedHashSet<>();
         for (String node : sortedNodes) {  // Iteração do dfs, seguindo agora a ordem gerada pelo dfs do grafo padrão.
             if (!visited.contains(node)) { // Mesmo processo, verificando se vertice já foi marcado como visitado.
                 List<String> component = new ArrayList<>(); // Inicializa componente
                 dfsTransposeVisit(node, transpose, visited, component); // Realiza dfs no vertice.
                 result.add(component); // Adiciona o componente gerado a partir do dfs no resultado final.
+                System.out.println("Componente da transposta ->> " + component);
             }
         }
         return result; // Retorna resultado final.
@@ -85,6 +88,7 @@ public class InstagramCluster {
      */
     private void dfsTransposeVisit(String node, Map<String, List<String>> transpose, Set<String> visited, List<String> component) {
         visited.add(node); // Marca o vertice como visitado.
+        System.out.println("Visitados da transposta ->> " + visited);
         component.add(node); // Adiciona o vertice no componente.
         for (String neighbor : transpose.get(node)) { // Itera os vizinhos do vertice.
             if (!visited.contains(neighbor)) { // Verifica se vizinho ja foi marcado como visitado.
@@ -105,23 +109,41 @@ public class InstagramCluster {
 
     public static void main(String[] args) {
         Map<String, List<String>> graph = new HashMap<>();
-        graph.put("a", Arrays.asList("d", "c", "e"));
-        graph.put("b", Collections.singletonList("e"));
-        graph.put("c", Arrays.asList("f", "g"));
-        graph.put("d", Arrays.asList("b", "c", "e"));
-        graph.put("e", Collections.singletonList("g"));
-        graph.put("f", Arrays.asList("d", "g"));
-        graph.put("g", Collections.singletonList("b"));
+        // Exemplo 01
+//        graph.put("a", Arrays.asList("d", "c", "e"));
+//        graph.put("b", Collections.singletonList("e"));
+//        graph.put("c", Arrays.asList("f", "g"));
+//        graph.put("d", Arrays.asList("b", "c", "e"));
+//        graph.put("e", Collections.singletonList("g"));
+//        graph.put("f", Arrays.asList("d", "g"));
+//        graph.put("g", Collections.singletonList("b"));
+
+        // Exemplo 02
+        graph.put("a", Arrays.asList("d", "b"));
+        graph.put("b", Arrays.asList("d", "e"));
+        graph.put("c", Collections.singletonList("a"));
+        graph.put("d", Collections.singletonList("c"));
+        graph.put("e", Collections.singletonList("f"));
+        graph.put("f", Collections.singletonList("g"));
+        graph.put("g", Collections.singletonList("e"));
+
         InstagramCluster instagram = new InstagramCluster(graph);
 
-        instagram.printGraph();
+        Map<String, List<String>> thisGraph = instagram.graph;
+        Map<String, List<String>> thisGraphTranspose = instagram.getTranspose();
+        System.out.println("Grafo padrão ->> " + thisGraph);
+        System.out.println("Grafo transposto ->> " + thisGraphTranspose);
 
-        System.out.println(instagram.dfs());
+        System.out.println("----- Passo a passo do dfs normal -----");
+        Set<String> dfs = instagram.dfs();
+        System.out.println("---------------------------------------");
 
-        System.out.println(instagram.graph);
-        System.out.println(instagram.getTranspose());
+        System.out.println("Ordem de Visitados no DFS com grafo padrão ->> " + dfs);
 
-        System.out.println(instagram.dfsTranspose(instagram.getTranspose(), instagram.dfs()));
+        System.out.println("----- Passo a passo do dfs da tranposta -----");
+        List<List<String>> dfsTransposta = instagram.dfsTranspose(thisGraphTranspose, dfs);
+        System.out.println("---------------------------------------------");
+        System.out.println("Componentes gerados no DFS do grafo transposto na ordem do primeiro dfs ->> " + dfsTransposta);
 
     }
 }
--- NotCommented/InstagramCluster.java
+++ NotCommented/InstagramCluster.java
@@ -1,3 +1,5 @@
+package NotCommented;
+
 import java.util.*;
 
 public class InstagramCluster {
@@ -9,7 +11,6 @@ public class InstagramCluster {
 
     private void orderGraph(Map<String, List<String>> graph) {
         TreeMap<String, List<String>> sortedGraph = new TreeMap<>(graph);
-        
         for (String key : sortedGraph.keySet()) {
             List<String> neighbors = sortedGraph.get(key);
             Collections.sort(neighbors);
@@ -18,29 +19,38 @@ public class InstagramCluster {
         this.graph = sortedGraph;
     }
 
-    public List<String> dfs() {
-
+    /**
+     * Primeiro DFS, retorna a lista na ordem de vertices visitados.
+     */
+    public Set<String> dfs() {
         LinkedList<String> finished = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new LinkedHashSet<>();
         for (String node : graph.keySet()) {
             if (!visited.contains(node)) {
                 dfsVisit(node, visited, finished);
             }
         }
-
-        return finished;
+        return visited;
     }
 
+    /**
+     * DFS Para os vertices que estão sendo visitados.
+     */
     private void dfsVisit(String node, Set<String> visited, LinkedList<String> finished) {
         visited.add(node);
+        System.out.println("Visitados ->> " + visited);
         for (String neighbor : graph.get(node)) {
             if (!visited.contains(neighbor)) {
                 dfsVisit(neighbor, visited, finished);
             }
         }
         finished.addFirst(node);
+        System.out.println("Finalizados ->> " + finished);
     }
 
+    /**
+     *  Retora a transposta do grafo.
+     */
     public Map<String, List<String>> getTranspose() {
         Map<String, List<String>> transpose = new HashMap<>();
         for (String node : graph.keySet()) {
@@ -54,21 +64,30 @@ public class InstagramCluster {
         return transpose;
     }
 
-    public List<List<String>> dfsTranspose(Map<String, List<String>> transpose, List<String> sortedNodes) {
+
+    /**
+     * Realiza DFS especifíca da transposta e retornando o resultado com os componentes finais.
+     */
+    public List<List<String>> dfsTranspose(Map<String, List<String>> transpose, Set<String> sortedNodes) {
         List<List<String>> result = new ArrayList<>();
-        Set<String> visited = new HashSet<>();
+        Set<String> visited = new LinkedHashSet<>();
         for (String node : sortedNodes) {
             if (!visited.contains(node)) {
                 List<String> component = new ArrayList<>();
                 dfsTransposeVisit(node, transpose, visited, component);
                 result.add(component);
+                System.out.println("Componente da transposta ->> " + component);
             }
         }
         return result;
     }
 
+    /**
+     * DFS Para cada vertice da transposta.
+     */
     private void dfsTransposeVisit(String node, Map<String, List<String>> transpose, Set<String> visited, List<String> component) {
         visited.add(node);
+        System.out.println("Visitados da transposta ->> " + visited);
         component.add(node);
         for (String neighbor : transpose.get(node)) {
             if (!visited.contains(neighbor)) {
@@ -89,23 +108,41 @@ public class InstagramCluster {
 
     public static void main(String[] args) {
         Map<String, List<String>> graph = new HashMap<>();
-        graph.put("a", Arrays.asList("d", "c", "e"));
-        graph.put("b", Collections.singletonList("e"));
-        graph.put("c", Arrays.asList("f", "g"));
-        graph.put("d", Arrays.asList("b", "c", "e"));
-        graph.put("e", Collections.singletonList("g"));
-        graph.put("f", Arrays.asList("d", "g"));
-        graph.put("g", Collections.singletonList("b"));
+        // Exemplo 01
+//        graph.put("a", Arrays.asList("d", "c", "e"));
+//        graph.put("b", Collections.singletonList("e"));
+//        graph.put("c", Arrays.asList("f", "g"));
+//        graph.put("d", Arrays.asList("b", "c", "e"));
+//        graph.put("e", Collections.singletonList("g"));
+//        graph.put("f", Arrays.asList("d", "g"));
+//        graph.put("g", Collections.singletonList("b"));
+
+        // Exemplo 02
+        graph.put("a", Arrays.asList("d", "b"));
+        graph.put("b", Arrays.asList("d", "e"));
+        graph.put("c", Collections.singletonList("a"));
+        graph.put("d", Collections.singletonList("c"));
+        graph.put("e", Collections.singletonList("f"));
+        graph.put("f", Collections.singletonList("g"));
+        graph.put("g", Collections.singletonList("e"));
+
         InstagramCluster instagram = new InstagramCluster(graph);
 
-        instagram.printGraph();
+        Map<String, List<String>> thisGraph = instagram.graph;
+        Map<String, List<String>> thisGraphTranspose = instagram.getTranspose();
+        System.out.println("Grafo padrão ->> " + thisGraph);
+        System.out.println("Grafo transposto ->> " + thisGraphTranspose);
 
-        System.out.println(instagram.dfs());
+        System.out.println("----- Passo a passo do dfs normal -----");
+        Set<String> dfs = instagram.dfs();
+        System.out.println("---------------------------------------");
 
-        System.out.println(instagram.graph);
-        System.out.println(instagram.getTranspose());
+        System.out.println("Ordem de Visitados no DFS com grafo padrão ->> " + dfs);
 
-        System.out.println(instagram.dfsTranspose(instagram.getTranspose(), instagram.dfs()));
+        System.out.println("----- Passo a passo do dfs da tranposta -----");
+        List<List<String>> dfsTransposta = instagram.dfsTranspose(thisGraphTranspose, dfs);
+        System.out.println("---------------------------------------------");
+        System.out.println("Componentes gerados no DFS do grafo transposto na ordem do primeiro dfs ->> " + dfsTransposta);
 
     }
 }
